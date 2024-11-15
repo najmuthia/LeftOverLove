6d0776a54f99ba20d0fd45661678da3ff8737be3
dayangalyssa <dayangalyssa1004@gmail.com>
2024-11-15 16:52:41 +0700

    Ini dayang

--- app/src/main/java/com/example/leftoverlove/MainActivity.kt
+++ app/src/main/java/com/example/leftoverlove/MainActivity.kt
@@ -46,6 +46,14 @@ fun IniRadit(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun IniDayangg(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
